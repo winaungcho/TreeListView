8cab0384bff5cb1a5531cacf3b03c1b409331f46
Win Aung Cho <54325207+winaungcho@users.noreply.github.com>
2019-11-30 21:23:18 +0630

    Update TreeItem.java

--- app/src/main/java/com/structsoftlab/treelistview/TreeItem.java
+++ app/src/main/java/com/structsoftlab/treelistview/TreeItem.java
@@ -1,4 +1,15 @@
 package com.structsoftlab.treelistview;
+/*
+ Simple project for the Tree List View.
+ It is free to use this file and associated files
+ for the educational use only as long as this block exist
+ unchanged. For the commercial use, 
+ contact with auther/developer of this project.
+ Developer: Win Aung Cho, winaungcho@gmail.com
+ StructSoftLab.com
+ 30-Nov, 2019
+ LICENSE:  https://github.com/winaungcho/TreeListView/blob/master/LICENSE
+ */
 import java.util.*;  
 
 public class TreeItem {
